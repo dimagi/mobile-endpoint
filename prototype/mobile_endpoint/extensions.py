79191a7a0e90e08a7750379db0c6a66feefe39a9
Simon Kelly <skelly@dimagi.com>
2015-07-01 14:33:18 +0200

    add auth primitives

--- prototype/mobile_endpoint/extensions.py
+++ prototype/mobile_endpoint/extensions.py
@@ -1,5 +1,28 @@
+from functools import wraps
 from flask.ext.redis import FlaskRedis
-import uuid
+from flask import request, Response
 
 redis_store = FlaskRedis()
 
+
+def check_auth(auth):
+    if auth.type == 'basic':
+        return auth.username == 'admin' and auth.password == 'secret'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
--- prototype/mobile_endpoint/views/receiver.py
+++ prototype/mobile_endpoint/views/receiver.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, jsonify
+from mobile_endpoint.extensions import requires_auth
 
 mod = Blueprint('receiver', __name__, url_prefix='/receiver')
 
 
 @mod.route('/')
+@requires_auth
 def index():
     return jsonify({'tasks': [{'a': 1}]})
