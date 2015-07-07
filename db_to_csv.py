93c2de9778858563686763928a04a22515ecf3e7
NoahCarnahan <noah.carnahan@gmail.com>
2015-07-07 14:19:45 -0400

    Don't sort every row before copying

--- db_to_csv.py
+++ db_to_csv.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import settings
+import uuid
 
 from utils import psql, execute_query
 
@@ -13,21 +14,40 @@ class RowToCSV(object):
         self.limit = limit
 
     def copy(self):
-        sql = "\COPY (SELECT {columns} FROM {table} ORDER BY random() LIMIT {limit}) TO '{dest}' WITH (FORMAT TEXT, DELIMITER ',')".format(
-            table=self.table,
-            columns=','.join(self.columns),
-            dest=self.dest,
+
+        # Erase the contents of the file
+        open(self.dest, 'w').close()
+
+        LOWEST_UUID = "00000000-0000-0000-0000-000000000000"
+        KEEP = 3
+
+        for i in xrange(self.limit):
+            got_a_row = False
+            while not got_a_row:
+                high_uuid = str(uuid.uuid4())
+                low_uuid = high_uuid[:KEEP] + LOWEST_UUID[KEEP:]
+                sql = "\COPY (SELECT {columns} FROM {table} WHERE id BETWEEN '{low_uuid}' and '{high_uuid}' ORDER BY id DESC LIMIT 1) TO stdout WITH (FORMAT TEXT, DELIMITER ',')".format(
+                    table=self.table,
+                    columns=','.join(self.columns),
+                    low_uuid=low_uuid,
+                    high_uuid=high_uuid,
+                )
+                cmd = psql(c=sql)
+                if cmd.stderr:
+                    print(cmd.stderr)
+                    break
+                else:
+                    if cmd.stdout:
+                        with open(self.dest, "a") as file:
+                            file.write(cmd.stdout)
+                        got_a_row = True
+                        if i+1 % 100 == 0:
+                            print("Copied {} of {} rows.".format(i, self.limit))
+
+        print("Successfully copied {limit} rows to {dest}".format(
             limit=self.limit,
-        )
-        cmd = psql(c=sql, _bg=True)
-        if cmd.stderr:
-            print(cmd.stderr)
-        else:
-            print(cmd.stdout)
-            print("Successfully copied {limit} rows to {dest}".format(
-                limit=self.limit,
-                dest=self.dest,
-            ))
+            dest=self.dest,
+        ))
 
 
 class FormToCSV(RowToCSV):
