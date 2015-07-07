3d63d9e5a6fa1cc106cb0ed096d117bebcc3bf87
Simon Kelly <skelly@dimagi.com>
2015-07-07 14:30:59 +0200

    move from processing to subpackage

---
+++ prototype/mobile_endpoint/form/form_processing.py
@@ -0,0 +1,156 @@
+from datetime import datetime
+from uuid import uuid4, UUID
+
+from flask import logging
+from werkzeug.exceptions import BadRequest
+
+from mobile_endpoint.form.models import XFormInstance, doc_types, XFormDuplicate
+from mobile_endpoint.utils import adjust_datetimes, get_with_lock, ReleaseOnError, LockManager
+import xml2json
+
+
+logger = logging.getLogger(__name__)
+
+INVALID_ID = lambda id: BadRequest('Form submitted with an invalid ID: {}'.format(id))
+
+FORM_PARAMETER_NAME = 'xml_submission_file'
+
+MULTIPART_FILENAME_ERROR = BadRequest((
+    'If you use multipart/form-data, please name your file %s.\n'
+    'You may also do a normal (non-multipart) post '
+    'with the xml submission as the request body instead.\n'
+) % FORM_PARAMETER_NAME)
+MULTIPART_EMPTY_PAYLOAD_ERROR = BadRequest((
+    'If you use multipart/form-data, the file %s'
+    'must not have an empty payload\n'
+) % FORM_PARAMETER_NAME)
+EMPTY_PAYLOAD_ERROR = BadRequest('Post may not have an empty body\n')
+
+
+def get_request_metadata(request):
+    """
+    received_on
+    date_header
+    path
+    submit_ip
+    openrosa_headers
+    """
+    return {
+        'last_sync_token': request.headers.get('last_sync_token', None)
+    }
+
+
+def get_instance_and_attachment(request):
+    attachments = {}
+    if 'multipart/form-data' in request.headers.get('Content-Type'):
+        # ODK submission; of the form
+        # $ curl --form 'xml_submission_file=@form.xml' $URL
+        try:
+            instance = request.files[FORM_PARAMETER_NAME].read().strip()
+        except KeyError:
+            raise MULTIPART_FILENAME_ERROR
+        else:
+            for key, item in request.files.items():
+                if key != FORM_PARAMETER_NAME:
+                    attachments[key] = item
+        if not instance:
+            raise MULTIPART_EMPTY_PAYLOAD_ERROR
+    else:
+        # j2me and touchforms; of the form
+        # $ curl -H 'Content-Type: application/xml' --data '@form.xml' $URL
+        instance = request.data
+        if not instance:
+            raise EMPTY_PAYLOAD_ERROR
+
+    return instance, attachments
+
+
+def create_xform(instance_xml, attachments, request_meta, dao):
+    json_form = _get_xform_json(instance_xml)
+    adjust_datetimes(json_form)
+
+    xform = XFormInstance(
+        # form has to be wrapped
+        {'form': json_form},
+        # other properties can be set post-wrap
+        xmlns=json_form.get('@xmlns'),
+        # _attachments=attachments_builder.to_json(),
+        received_on=datetime.utcnow(),
+    )
+
+    for key, value in request_meta.items():
+        setattr(xform, key, value)
+
+    id_from_xml = _extract_meta_field(json_form, ('instanceID', 'uuid'))
+    form_id = id_from_xml or uuid4().hex
+    xform['id'] = form_id
+    xform_lock = aquire_xform_lock(xform)
+    with ReleaseOnError(xform_lock.lock):
+        if id_from_xml:
+            try:
+                UUID(form_id)
+            except ValueError:
+                raise INVALID_ID(form_id)
+            else:
+                existing_form = dao.get_form(form_id)
+                if existing_form:
+                    return handle_duplicate_form(xform_lock, existing_form)
+
+    return xform_lock
+
+
+def aquire_xform_lock(instance):
+    return get_with_lock('xform-process-lock-{}'.format(instance['id']), lambda: instance)
+
+
+def handle_duplicate_form(xform_lock, existing_form):
+    new_form, lock = xform_lock
+    conflict_id = new_form['id']
+    if new_form['domain'] != existing_form['domain'] or existing_form['doc_type'] not in doc_types():
+        # just change the ID and continue
+        new_form['id'] = uuid4().hex
+        return xform_lock
+    else:
+        # do some fancy stuff here by comparing xform md5 hashes etc.
+        # for now assume that the md5's are the same
+        new_form['id'] = uuid4().hex
+        new_form['doc_type'] = 'XFormDuplicate'
+        dupe = XFormDuplicate.wrap(new_form.to_json())
+        dupe['problem'] = "Form is a duplicate of another! (%s)" % conflict_id
+        return LockManager(dupe, lock)
+
+
+def _get_xform_json(xml_string):
+    name, json_form = xml2json.xml2json(xml_string)
+    json_form['#type'] = name
+    return json_form
+
+
+def _extract_meta_field(form, fields):
+    """Takes form json (as returned by xml2json)"""
+    if form.get('Meta'):
+        # bhoma, 0.9 commcare
+        meta = form['Meta']
+    elif form.get('meta'):
+        # commcare 1.0
+        meta = form['meta']
+    else:
+        return None
+
+    for field in fields:
+        if field in meta:
+            return meta[field]
+
+
+
+def is_deprecation(xform):
+    return xform.doc_type == deprecation_type()
+
+
+def deprecation_type():
+    return 'XFormDeprecated'
+
+
+def is_override(xform):
+    # it's an override if we've explicitly set the "deprecated_form_id" property on it.
+    return bool(getattr(xform, 'deprecated_form_id', None))
---
+++ prototype/mobile_endpoint/form/models.py
@@ -0,0 +1,190 @@
+from __future__ import absolute_import
+
+import datetime
+import hashlib
+from copy import copy
+
+from jsonobject.api import re_date, JsonObject
+from jsonobject.base import DefaultProperty
+from jsonobject.properties import DateTimeProperty, StringProperty, DictProperty, BooleanProperty, ListProperty
+from mobile_endpoint.form import const
+
+
+def doc_types():
+    """
+    Mapping of doc_type attributes in CouchDB to the class that should be instantiated.
+    """
+    return {
+        'XFormInstance': XFormInstance,
+        # 'XFormArchived': XFormArchived,
+        # 'XFormDeprecated': XFormDeprecated,
+        'XFormDuplicate': XFormDuplicate,
+        'XFormError': XFormError,
+        # 'SubmissionErrorLog': SubmissionErrorLog,
+    }
+
+
+class Metadata(JsonObject):
+    """
+    Metadata of an xform, from a meta block structured like:
+
+        <Meta>
+            <timeStart />
+            <timeEnd />
+            <instanceID />
+            <userID />
+            <deviceID />
+            <deprecatedID /> 
+            <username />
+
+            <!-- CommCare extension -->
+            <appVersion />
+            <location />
+        </Meta>
+
+    See spec: https://bitbucket.org/javarosa/javarosa/wiki/OpenRosaMetaDataSchema
+
+    username is not part of the spec but included for convenience
+    """
+    timeStart = DateTimeProperty()
+    timeEnd = DateTimeProperty()
+    instanceID = StringProperty()
+    userID = StringProperty()
+    deviceID = StringProperty()
+    deprecatedID = StringProperty()
+    username = StringProperty()
+    appVersion = StringProperty()
+    # location = GeoPointProperty()
+
+
+class XFormOperation(JsonObject):
+    """
+    Simple structure to represent something happening to a form.
+
+    Currently used just by the archive workflow.
+    """
+    user = StringProperty()
+    date = DateTimeProperty(default=datetime.datetime.utcnow)
+    operation = StringProperty()  # e.g. "archived", "unarchived"
+
+
+class XFormInstance(JsonObject):
+    """An XForms instance."""
+    doc_type = 'XFormInstance'
+    domain = StringProperty()
+    app_id = StringProperty()
+    xmlns = StringProperty()
+    form = DictProperty()
+    received_on = DateTimeProperty()
+    # Used to tag forms that were forcefully submitted
+    # without a touchforms session completing normally
+    partial_submission = BooleanProperty(default=False)
+    history = ListProperty(XFormOperation)
+    auth_context = DictProperty()
+    submit_ip = StringProperty()
+    path = StringProperty()
+    openrosa_headers = DictProperty()
+    last_sync_token = StringProperty()
+    # almost always a datetime, but if it's not parseable it'll be a string
+    date_header = DefaultProperty()
+    build_id = StringProperty()
+    export_tag = DefaultProperty(name='#export_tag')
+
+    @property
+    def type(self):
+        return self.form.get(const.TAG_TYPE, "")
+        
+    @property
+    def name(self):
+        return self.form.get(const.TAG_NAME, "")
+
+    @property
+    def version(self):
+        return self.form.get(const.TAG_VERSION, "")
+        
+    @property
+    def uiversion(self):
+        return self.form.get(const.TAG_UIVERSION, "")
+
+    @property
+    def metadata(self):
+        def get_text(node):
+            if node is None:
+                return None
+            if isinstance(node, dict) and '#text' in node:
+                value = node['#text']
+            elif isinstance(node, dict) and all(a.startswith('@') for a in node):
+                return None
+            else:
+                value = node
+
+            if not isinstance(value, basestring):
+                value = unicode(value)
+            return value
+
+        if const.TAG_META in self.form:
+            def _clean(meta_block):
+                ret = copy(dict(meta_block))
+                for key in ret.keys():
+                    # remove attributes from the meta block
+                    if key.startswith('@'):
+                        del ret[key]
+
+                # couchdbkit erroneously converts appVersion to a Decimal just because it is possible (due to it being within a "dynamic" property)
+                # (see https://github.com/benoitc/couchdbkit/blob/a23343e539370cffcf8b0ce483c712911bb022c1/couchdbkit/schema/properties.py#L1038)
+                ret['appVersion'] = get_text(meta_block.get('appVersion'))
+                ret['location'] = get_text(meta_block.get('location'))
+
+                # couchdbkit chokes on dates that aren't actually dates
+                # so check their validity before passing them up
+                if meta_block:
+                    for key in ("timeStart", "timeEnd"):
+                        if key in meta_block:
+                            if meta_block[key]:
+                                if re_date.match(meta_block[key]):
+                                    # this kind of leniency is pretty bad
+                                    # and making it midnight in UTC
+                                    # is totally arbitrary
+                                    # here for backwards compatibility
+                                    meta_block[key] += 'T00:00:00.000000Z'
+                                # try:
+                                #     # try to parse to ensure correctness
+                                #     parsed = iso_string_to_datetime(meta_block[key])
+                                #     # and set back in the right format in case it was a date, not a datetime
+                                #     ret[key] = json_format_datetime(parsed)
+                                # except BadValueError:
+                                #     # we couldn't parse it
+                                #     del ret[key]
+                            else:
+                                # it was empty, also a failure
+                                del ret[key]
+                    # also clean dicts on the return value, since those are not allowed
+                    for key in ret:
+                        if isinstance(ret[key], dict):
+                            ret[key] = ", ".join(\
+                                "%s:%s" % (k, v) \
+                                for k, v in ret[key].items())
+                return ret
+            return Metadata.wrap(_clean(self.to_json()[const.TAG_FORM][const.TAG_META]))
+
+        return None
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.type, self.xmlns)
+
+    def xml_md5(self):
+        return hashlib.md5(self.get_xml().encode('utf-8')).hexdigest()
+    
+
+class XFormError(XFormInstance):
+    """
+    Instances that have errors go here.
+    """
+    doc_type = 'XFormError'
+    problem = StringProperty()
+    orig_id = StringProperty()
+
+        
+class XFormDuplicate(XFormError):
+    doc_type = 'XFormDuplicate'
+    pass
