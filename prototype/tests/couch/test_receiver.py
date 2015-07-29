bf9e92c84b06defbca8e3206142acd815a1e63c6
Cory Zue <czue@dimagi.com>
2015-07-29 14:12:28 +0200

    flesh out couch receiver case tests

--- prototype/tests/couch/test_receiver.py
+++ prototype/tests/couch/test_receiver.py
@@ -1,5 +1,5 @@
 import pytest
-from mobile_endpoint.backends.couch.models import CouchForm
+from mobile_endpoint.backends.couch.models import CouchForm, CouchCase
 from mobile_endpoint.models import FormData, CaseData, Synclog
 from mobile_endpoint.synclog.checksum import Checksum
 from tests.conftest import couch
@@ -25,9 +25,17 @@ class TestCouchReceiver(ReceiverTestMixin):
             assert form.synclog_id is None
 
     def _assert_case(self, case_id, owner_id, num_forms=1, closed=False, indices=None):
-        # todo
-        pass
+        couch_case = CouchCase.get(case_id)
+        assert couch_case is not None
+        assert couch_case.domain == DOMAIN
+        assert couch_case.closed == closed
+        # assert len(couch_case.forms) == num_forms
+        assert couch_case.owner_id == owner_id
+
+        return couch_case
 
     def _assert_synclog(self, id, case_ids=None, dependent_ids=None, index_tree=None):
         # todo
         pass
+
+
