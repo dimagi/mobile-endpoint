fbec62f8e2483267457aa0997e0ef18cdf533a9c
NoahCarnahan <noah.carnahan@gmail.com>
2015-07-30 18:06:22 -0400

    [WIP] abstract synclog retreival in restore tests

--- prototype/tests/sql/test_restore.py
+++ prototype/tests/sql/test_restore.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from mobile_endpoint.backends.manager import BACKEND_SQL
+from mobile_endpoint.models import Synclog
 from tests.conftest import sql
 from tests.test_restore import RestoreTestMixin
 
@@ -11,3 +12,12 @@ class TestSQLRestore(RestoreTestMixin):
 
     def _get_backend(self):
         return BACKEND_SQL
+
+    def _get_all_synclogs(self):
+        return Synclog.query.all()
+
+    def _get_one_synclog(self):
+        return Synclog.query.one()
+
+    def _get_synclog_by_previous_id(self, id):
+        return Synclog.query.filter(Synclog.previous_log_id == id).one()
