d0d45784c451d9a589ac5591bdf2b43c86e89aa5
NoahCarnahan <noah.carnahan@gmail.com>
2015-07-30 18:59:20 -0400

    [WIP] progress on mongo restore tests

--- prototype/tests/mongo/test_restore.py
+++ prototype/tests/mongo/test_restore.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+from uuid import UUID
 import pytest
 from mobile_endpoint.backends.manager import BACKEND_MONGO
+from mobile_endpoint.backends.mongo.models import MongoSynclog
 from tests.conftest import mongo
 from tests.test_restore import RestoreTestMixin
+
+
+class MongoSyncLog(object):
+    pass
 
 
 @pytest.mark.usefixtures("testapp", "client", "mongodb", "db_reset")
@@ -11,3 +17,12 @@ class TestCouchRestore(RestoreTestMixin):
 
     def _get_backend(self):
         return BACKEND_MONGO
+
+    def _get_one_synclog(self):
+        return MongoSynclog.objects.first()
+
+    def _get_synclog_by_previous_id(self, id):
+        MongoSynclog.objects(previous_log_id=UUID(id)).first()
+
+    def _get_all_synclogs(self):
+        return MongoSynclog.objects.all()
