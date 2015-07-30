5d8fff91d69291e9307d24acbc16015e1fe78918
NoahCarnahan <noah.carnahan@gmail.com>
2015-07-30 09:02:41 -0400

    Add mongo uri settings

--- prototype/instance/testconfig.example.py
+++ prototype/instance/testconfig.example.py
@@ -5,3 +5,5 @@ TESTING = True
 SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:password@localhost/mobile_endpoint'
 
 COUCH_URI = 'http://localhost:5984'
+
+MONGO_URI = 'mongodb://localhost/mobile_endpoint_test'
