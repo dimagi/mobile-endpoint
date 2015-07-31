022cd180f798694f2c045b7be2fed6de75782ead
NoahCarnahan <noah.carnahan@gmail.com>
2015-07-31 14:34:42 -0400

    Return generic synclogs from test helpers

--- prototype/tests/mongo/test_restore.py
+++ prototype/tests/mongo/test_restore.py
@@ -15,13 +15,13 @@ class TestMongoRestore(RestoreTestMixin):
         return BACKEND_MONGO
 
     def _get_one_synclog(self):
-        return MongoSynclog.objects.first()
+        return MongoSynclog.objects.first().to_generic()
 
     def _get_synclog_by_previous_id(self, id):
-        MongoSynclog.objects(previous_log_id=UUID(id)).first()
+        return MongoSynclog.objects(previous_log_id=UUID(id)).first().to_generic()
 
     def _get_all_synclogs(self):
-        return MongoSynclog.objects.all()
+        return [l.to_generic() for l in MongoSynclog.objects.all()]
 
     def _get_restore_url_snippet(self):
         return 'mongo-restore'
