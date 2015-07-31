f37fa1f7f7a1876d72e393f95b9432d6f6130357
Simon Kelly <skelly@dimagi.com>
2015-07-31 15:55:35 +0200

    move imports below patching

--- prototype/server.py
+++ prototype/server.py
@@ -1,24 +1,18 @@
-import os
+# Set up psycopg2 & SQLAlchemy to be greenlet-friendly.
+# Note: psycogreen does not really monkey patch psycopg2 in the
+# manner that gevent monkey patches socket.
+#
+from restkit.session import set_session
+set_session("gevent")
+from gevent.monkey import patch_all
+patch_all()
+from psycogreen.gevent import patch_psycopg
+patch_psycopg()
+
 from patch_path import patch_path
 patch_path()
 
 from mobile_endpoint import create_app
-
-# Optionally, set up psycopg2 & SQLAlchemy to be greenlet-friendly.
-# Note: psycogreen does not really monkey patch psycopg2 in the
-# manner that gevent monkey patches socket.
-#
-if "PSYCOGREEN" in os.environ:
-
-    # Do our monkey patching
-    #
-    from restkit.session import set_session
-    set_session("gevent")
-    from gevent.monkey import patch_all
-    patch_all()
-    from psycogreen.gevent import patch_psycopg
-    patch_psycopg()
-
 
 app = create_app()
 
